69ccf7ac2b1b538d94e45437f221d36f192c59c9
Andrew Ewing <drew@phenocode.com>
2020-11-12 10:56:15 -0700

    Initial import

---
+++ [...slug].svelte
@@ -0,0 +1,15 @@
+<script>
+  import features from "$features/index";
+  import Layout from "sveature-layout";
+  import { parseFeatures, getTargetFromPage } from "sveature";
+  export let promise = parseFeatures(features);
+  import { page } from "$app/stores";
+
+  $: [component, feature] = getTargetFromPage($page);
+</script>
+
+{#await promise}
+  <strong>Loading...</strong>
+{:then components}
+  <Layout {components} {component} {feature} />
+{/await}
---
+++ index.svelte
@@ -0,0 +1,13 @@
+<script>
+  import features from "$features/index";
+  import Layout from "sveature-layout";
+  import { parseFeatures } from "sveature";
+  export let promise = parseFeatures(features);
+</script>
+
+{#await promise then components}
+  <Layout {components}>
+    <h1>Design Library</h1>
+    <p>Welcome to Sveature!</p>
+  </Layout>
+{/await}
